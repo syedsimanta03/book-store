cc0fb16da2da9e63bda5e3761a5d4a075b2030d8
Syed Simanta <syed.simanta06@gmail.com>
2020-02-20 21:42:25 +0600

    'proxy'

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,4 +1,5 @@
-const proxy = require('http-proxy-middleware');
+const { proxy } = require('http-proxy-middleware');
+
 
     module.exports = app => {
       app.use(proxy(['/books/', '/users/', '/authors/', '/booksinstances/', '/genres/**'], { target: 'http://localhost:5000/' }))};
